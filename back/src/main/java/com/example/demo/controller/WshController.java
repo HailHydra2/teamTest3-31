100ca0154451adcaeece90ac6131fa4995b5a8c2
fzuwsh <1306321810@qq.com>
2020-03-31 21:29:24 +0800

    wsh2

--- back/src/main/java/com/example/demo/controller/WshController.java
+++ back/src/main/java/com/example/demo/controller/WshController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.model.AjaxResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,4 @@ public class WshController {
         return "hello";
     }
 
-    
 }
---
+++ back/src/main/java/com/example/demo/model/user.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class user {
+    private String name;
+    private int age;
+}
