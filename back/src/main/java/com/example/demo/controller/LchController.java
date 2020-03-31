08748a8398adc9c71832c30e96aa23ea4b77fcbe
liuchenghua <547173318@qq.com>
2020-03-31 22:23:51 +0800

    Create LchController.java

---
+++ back/src/main/java/com/example/demo/controller/LchController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.AjaxResponse;
+import com.example.demo.model.Article;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/lch")
+public class LchController {
+
+    @PutMapping("/hello1")
+    public @ResponseBody
+    AjaxResponse updateArticle(@RequestBody Article article) {
+
+        /*article.setId(id);
+        log.info("updateArticle：{}",article);
+        return AjaxResponse.success(article);
+         */
+        return AjaxResponse.success(article);
+
+    }
+
+    @PostMapping("/user")
+    public @ResponseBody AjaxResponse saveArticle(@RequestBody user use) {
+        log.info("saveArticle:{}",use);
+
+        use.name="liuchenghua"
+        return AjaxResponse.success(use);
+
+    }
+}
