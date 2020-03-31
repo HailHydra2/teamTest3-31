6593e03c16e956c93cded50999e19b16f5c609c1
fzuwsh <1306321810@qq.com>
2020-03-31 22:28:55 +0800

    wsh2

--- back/src/main/java/com/example/demo/controller/WshController.java
+++ back/src/main/java/com/example/demo/controller/WshController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.AjaxResponse;
+import com.example.demo.model.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,14 @@ public class WshController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping("/user")
+    public AjaxResponse wshControll(@RequestBody user u){
+        log.info("user {}", u);
+        u.setName("wsh");
+        u.setAge(20);
+        return AjaxResponse.success(u);
     }
 
 }
